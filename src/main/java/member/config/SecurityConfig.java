b89d38203a1b600dee805f8350bd17450fcfe17f
yujong <yujong4170@gmail.com>
2022-11-03 09:45:15 +0900

    aaa

--- src/main/java/member/config/SecurityConfig.java
+++ src/main/java/member/config/SecurityConfig.java
@@ -1,58 +1,45 @@
 package member.config;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import member.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
-@RequiredArgsConstructor
-public class SecurityConfig  {
-
-    public MemberService memberService;
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private MemberService memberService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    /*
-     * https://scribnote5.github.io/posts/Project-Lab10/ 참고
-     * 비밀번호 암호화에 사용될 PasswordEncoder를 BCryptPasswordEncoder로 사용
-     * BCryptPasswordEncoder는 해시 뿐만 아니라 Salt를 넣는 작업을 수행하므로, 입력 값이 같음에도 매번 다른 encoded된 값을 반환함
-     *
-     * @return
-     */
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
+    @Override
+    public void configure(WebSecurity web) throws Exception {
         // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
-        return (web) -> web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/h2-console/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/h2/**");
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.headers().frameOptions().sameOrigin();
-        http.csrf().ignoringAntMatchers("/h2-console/**").disable();
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 // 페이지 권한 설정
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/myinfo").hasRole("MEMBER")
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/h2-console/**").permitAll()
+                .antMatchers("/user/my-info").hasRole("MEMBER")
+                .antMatchers("/h2/**").permitAll()
                 .and() // 로그인 설정
                 .formLogin()
                 .loginPage("/user/login")
@@ -64,15 +51,15 @@ public class SecurityConfig  {
                 .logoutSuccessUrl("/user/logout/result")
                 .invalidateHttpSession(true)
                 .and()
-                // 403 예외처리 핸들링
-                .exceptionHandling().accessDeniedPage("/user/denied");
+                .csrf().disable()
+                .headers().frameOptions().disable();
 
-                return http.build();
+                // 403 예외처리 핸들링
+
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
     }
 }
